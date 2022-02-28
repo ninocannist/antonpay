84c0b2448562796d41f93fe5ed6ee2af3f628d5b
Antonino Cannistraci <67139866+antoninocanni@users.noreply.github.com>
2022-02-28 23:22:34 +0100

    Add cors for express

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,30 @@
-import express from 'express';
-import router from './lib/router';
-import path from 'path';
+import express from 'express'
+import router from './lib/router'
+import path from 'path'
+import cors from 'cors'
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3001 } = process.env
 
-const app = express();
+const app = express()
+app.use(cors())
 
 // Middleware that parses json and looks at requests where the Content-Type header matches the type option.
-app.use(express.json());
+app.use(express.json())
 
 // Serve API requests from the router
-app.use('/api', router);
+app.use('/api', router)
 
 // Serve storybook production bundle
-app.use('/storybook', express.static('dist/storybook'));
+app.use('/storybook', express.static('dist/storybook'))
 
 // Serve app production bundle
-app.use(express.static('dist/app'));
+app.use(express.static('dist/app'))
 
 // Handle client routing, return all requests to the app
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'app/index.html'));
-});
+  res.sendFile(path.join(__dirname, 'app/index.html'))
+})
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
